601dd13fbf4818830534cb1880e43c540d0033aa
Md Abdul Kuddus <ma.kuddus37@gmail.com>
2023-05-09 19:25:52 +0600

    added version

---
+++ public/build/assets/Blank-2859198b.js
@@ -0,0 +1,1 @@
+import{a,F as r}from"./app-f70fd78f.js";function n(){return a(r,{})}export{n as default};
--- public/build/assets/Blank-ccd28bcb.js
+++
@@ -1,1 +0,0 @@
-import{a,F as r}from"./app-ec696ba5.js";function n(){return a(r,{})}export{n as default};
