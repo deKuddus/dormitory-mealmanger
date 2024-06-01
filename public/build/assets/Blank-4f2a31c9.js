8e7fa0d859250e049373a58a7fab5c5e62ec066d
Md Abdul Kuddus <ma.kuddus37@gmail.com>
2024-06-01 14:16:39 +0600

    build

--- public/build/assets/Blank-4f2a31c9.js
+++
@@ -1,1 +0,0 @@
-import{a,F as r}from"./app-42a37551.js";function n(){return a(r,{})}export{n as default};
---
+++ public/build/assets/Blank-789e4bf4.js
@@ -0,0 +1,1 @@
+import{a,F as r}from"./app-9b1aff9a.js";function n(){return a(r,{})}export{n as default};
